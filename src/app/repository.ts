26d7f6011bd393871b42926a8575d541666e18ce
Moses Karani <karanim594@gmail.com>
2022-04-18 14:32:12 +0300

    created a repository class

---
+++ src/app/repository.ts
@@ -0,0 +1,5 @@
+export class Repository {
+    constructor(public name:string, public full_name:string, public description: string, public updated_on:Date, public html_url:string) {
+
+    }
+}
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,5 @@
 export class User {
-    constructor(public bio: string, public repos: number, public login:string, public avatar_url: string, public created_on:Date, public html_url: string) {}
+    constructor(public bio: string, public repos: number, public login:string, public avatar_url: string, public created_on:Date, public html_url: string) {
+        
+    }
 }
