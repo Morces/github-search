f1ec2bedf4362025788eda2ba0d809b0fd0944f3
Moses Karani <karanim594@gmail.com>
2022-04-19 08:32:37 +0300

    routing optimization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { SearchComponent } from './components/search/search.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Users", component:SearchComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/days.pipe.ts
+++ src/app/days.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DaysPipe implements PipeTransform {
 
   transform(value: any): any {
-    let arrVal = value.replace(/(\d{4})- (\d{2}).*/ig,"$1 $2 $3").split(" ");
+    let arrVal = value.replace(/(\d{4})- (\d{2}) - (\d{2}).*/ig,"$1 $2 $3").split(" ");
     let dateCreated = new Date(arrVal[0], arrVal[1]-1, arrVal[2]);
     let now = new Date();
     let dayDiff = now.getTime() - dateCreated.getTime();
